9cf90217d3ef831d6285b87fc320a8c3ad4bdf07
Andrey Novik <novik.93@bk.ru>
2022-12-07 18:49:47 +0300

    homework5

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,9 @@
-import React, { useState, useEffect } from "react";
-import api from "./api";
+import React from "react";
+
 import Users from "./component/users";
 
 function App() {
-    const [users, setUsers] = useState();
-    useEffect(() => {
-        api.users.fetchAll().then((data) => setUsers(data));
-    }, []);
-    const handleDelete = (userId) => {
-        setUsers(users.filter((user) => user._id !== userId));
-    };
-    const handleToggleBookMark = (id) => {
-        setUsers(
-            users.map((user) => {
-                if (user._id === id) {
-                    return { ...user, bookmark: !user.bookmark };
-                }
-                return user;
-            })
-        );
-    };
-    return (
-        <div>
-            {users && (
-                <Users
-                    onDelete={handleDelete}
-                    onToggleBookMark={handleToggleBookMark}
-                    users={users}
-                />
-            )}
-        </div>
-    );
+    return <Users />;
 }
 
 export default App;
---
+++ src/component/table.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+
+const Table = ({
+    onSort,
+    selectedSort,
+    columns,
+    data,
+    children,
+    renderCaret
+}) => {
+    return (
+        <table className="table">
+            {children || (
+                <>
+                    <TableHeader
+                        {...{ onSort, selectedSort, columns, renderCaret }}
+                    />
+                    <TableBody {...{ columns, data }} />
+                </>
+            )}
+        </table>
+    );
+};
+Table.propTypes = {
+    onSort: PropTypes.func,
+    selectedSort: PropTypes.object,
+    columns: PropTypes.object,
+    data: PropTypes.array,
+    children: PropTypes.array,
+    renderCaret: PropTypes.func
+};
+export default Table;
